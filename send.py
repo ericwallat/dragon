bf1188804a2277b863d0d9ef2cec94c7343ec82d
ericwallat <30661023+ericwallat@users.noreply.github.com>
2017-09-07 10:16:45 -0500

    Changed flask server

--- send.py
+++ send.py
@@ -24,14 +24,16 @@ CORS(app)
 
 @app.route("/", methods=['GET', 'POST'])
 def test():
-	data = {}
+	data = {1: "rgb(255,255,255)",2: "rgb(255,255,255)",3: "rgb(255,255,255)",4: "rgb(255,255,255)",
+		5: "rgb(255,255,255)",6: "rgb(255,255,255)",7: "rgb(255,255,255)",8: "rgb(255,255,255)",
+		9: "rgb(255,255,255)",10: "rgb(255,255,255)",11: "rgb(255,255,255)",12: "rgb(255,255,255)",
+		13: "rgb(255,255,255)"}
 	if request.method == 'POST':
 		parser = argparse.ArgumentParser()
 		parser.add_argument("--ip", default="ewallat7-32987",help="The ip of the OSC server")
 		parser.add_argument("--port", type=int, default=8005,help="The port the OSC server is listening on")
 		args = parser.parse_args()
 		client = udp_client.SimpleUDPClient(args.ip, args.port)
-		print(request.json)
 		if request.json['play']:
 			client.send_message("/cs/key/clearall", " ")
 			client.send_message("/cs/playback/go", " ")
@@ -56,13 +58,20 @@ def test():
 			channelChange = "/cs/chan/select/" + request.json['chan']
 			print("Changing channel selection to channel " + request.json['chan'])
 			client.send_message(channelChange," ")
-			print("Changing hsv to " + str(request.json['hue']) + ", " + str(request.json['sat']) + ", " + str(request.json['val']))
-			level = "/cs/chan/at/" + str(request.json['val'])
+			h = request.json['hue']
+			s = request.json['sat']
+			v = request.json['val']
+			print("Changing hsv to " + str(h) + ", " + str(s) + ", " + str(v))
+			level = "/cs/chan/at/" + str(v)
 			client.send_message(level," ")
-			huesat = "/cs/color/hs/" + str(request.json['hue']) + "/" + str(request.json['sat'])
+			huesat = "/cs/color/hs/" + str(h) + "/" + str(s)
 			client.send_message(huesat," ")
 			client.send_message("/cs/playback/go", " ")
 			client.send_message("/cs/playback/pause", " ")
+			with open('data.json', 'r') as jsonFile:
+				data = json.load(jsonFile)
+			rgb = tuple(int(i * 255) for i in colorsys.hsv_to_rgb(h/360,s/100,v/100))
+			data[request.json['chan']] = "rgb" + str(rgb)
 		client.send_message("/cs/key/clearselection", " ")
 		with open('data.json', 'w') as outfile:
 			json.dump(data, outfile)
@@ -70,4 +79,4 @@ def test():
 	else:
 		return('Complete')
 if __name__== "__main__":
-	app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0', port=8006)
