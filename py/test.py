3687ee69b487512f5262251c53b5629c876b3331
Eric Wallat <eric.wallat@etcconnect.com>
2017-09-12 12:54:50 -0500

    initial commit from stream

--- py/test.py
+++ py/test.py
@@ -1,18 +1,32 @@
-from flask import Flask
-from flask_cors import CORS, cross_origin
-from flask import request
-from flask import jsonify
-import json
+import argparse
+import math
 
+from pythonosc import dispatcher
+from pythonosc import osc_server
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--ip", default="192.168.102.55",help="The ip of the OSC server")
+parser.add_argument("--port", type=int, default=8006,help="The port the OSC server is listening on")
+args = parser.parse_args()
+dispatcher = dispatcher.Dispatcher()
+dispatcher.map("*", print)
+server = osc_server.ThreadingOSCUDPServer(
+      (args.ip, args.port), dispatcher)
+
+def print_volume_handler(unused_addr, args, volume):
+  print("[{0}] ~ {1}".format(args[0], volume))
+
+def print_compute_handler(unused_addr, args, volume):
+  try:
+    print("[{0}] ~ {1}".format(args[0], args[1](volume)))
+  except ValueError: pass
+
+if __name__== "__main__":
+	app.run(host='0.0.0.0', port=6000)
+	
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/", methods=['GET', 'POST'])
-
 def test():
-    with open('data.json', 'w') as outfile:
-        json.dump(request.json, outfile)
-    return("complete")
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+  print(request.json)
--- send.py
+++ send.py
@@ -8,36 +8,59 @@ import time
 import json
 import threading
 import colorsys
+import datetime, calendar
+from collections import deque
 
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
 
 
+lastClick = 0
+log = ''
+d=deque([], maxlen=5)
+playing = False;
+parser = argparse.ArgumentParser()
+parser.add_argument("--ip", default="192.168.102.55",help="The ip of the OSC server")
+parser.add_argument("--port", type=int, default=8005,help="The port the OSC server is listening on")
+args = parser.parse_args()
+client = udp_client.SimpleUDPClient(args.ip, args.port)
+data = {1: "rgb(255,255,255)",2: "rgb(255,255,255)",3: "rgb(255,255,255)",4: "rgb(255,255,255)",
+		5: "rgb(255,255,255)",6: "rgb(255,255,255)",7: "rgb(255,255,255)",8: "rgb(255,255,255)",
+		9: "rgb(255,255,255)",10: "rgb(255,255,255)",11: "rgb(255,255,255)",12: "rgb(255,255,255)",
+		13: "rgb(255,255,255)"}
 
 def sendData():
 	print("Hello! Dragon OSC Server is up.")
 sendData()
 
 
+def checkclick():
+	global lastClick, playing, data
+	threading.Timer(2, checkclick).start()
+	if lastClick <= 1000*calendar.timegm(time.gmtime()) - 60000 and not playing:
+		client.send_message("/cs/key/clearall", " ")
+		client.send_message("/cs/playback/go", " ")
+		print("Playing cues...")
+		playing = True
+		data = {1: "rgb(255,255,255)",2: "rgb(255,255,255)",3: "rgb(255,255,255)",4: "rgb(255,255,255)",
+		5: "rgb(255,255,255)",6: "rgb(255,255,255)",7: "rgb(255,255,255)",8: "rgb(255,255,255)",
+		9: "rgb(255,255,255)",10: "rgb(255,255,255)",11: "rgb(255,255,255)",12: "rgb(255,255,255)",
+		13: "rgb(255,255,255)"}
+		with open('json/data.json', 'w') as outfile:
+			json.dump(data, outfile)
+checkclick()
+
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/", methods=['GET', 'POST'])
 def test():
-	data = {1: "rgb(255,255,255)",2: "rgb(255,255,255)",3: "rgb(255,255,255)",4: "rgb(255,255,255)",
-		5: "rgb(255,255,255)",6: "rgb(255,255,255)",7: "rgb(255,255,255)",8: "rgb(255,255,255)",
-		9: "rgb(255,255,255)",10: "rgb(255,255,255)",11: "rgb(255,255,255)",12: "rgb(255,255,255)",
-		13: "rgb(255,255,255)"}
 	if request.method == 'POST':
-		parser = argparse.ArgumentParser()
-		parser.add_argument("--ip", default="ewallat7-32987",help="The ip of the OSC server")
-		parser.add_argument("--port", type=int, default=8005,help="The port the OSC server is listening on")
-		args = parser.parse_args()
-		client = udp_client.SimpleUDPClient(args.ip, args.port)
-		if request.json['play']:
-			client.send_message("/cs/key/clearall", " ")
-			client.send_message("/cs/playback/go", " ")
-		elif request.json['preset']:
+		global playing, lastClick, data
+		lastClick = request.json['last']
+		if not request.json['play']:
+			playing = False
+		if request.json['preset']:
 			client.send_message("/cs/playback/go", " ")
 			client.send_message("/cs/playback/pause", " ")
 			i = 1
@@ -54,7 +77,11 @@ def test():
 				client.send_message("/cs/chan/at/" + str(v), " ")
 				client.send_message("/cs/color/hs/" + str(h) + "/" + str(s), " ")
 				i = i+1
+			log = "Changing channels for " + request.json['theme'] + " theme... "
+			d.append(log)
 		else:
+			client.send_message("/cs/playback/go", " ")
+			client.send_message("/cs/playback/pause", " ")
 			channelChange = "/cs/chan/select/" + request.json['chan']
 			print("Changing channel selection to channel " + request.json['chan'])
 			client.send_message(channelChange," ")
@@ -66,17 +93,20 @@ def test():
 			client.send_message(level," ")
 			huesat = "/cs/color/hs/" + str(h) + "/" + str(s)
 			client.send_message(huesat," ")
-			client.send_message("/cs/playback/go", " ")
-			client.send_message("/cs/playback/pause", " ")
-			with open('data.json', 'r') as jsonFile:
+			with open('json/data.json', 'r') as jsonFile:
 				data = json.load(jsonFile)
 			rgb = tuple(int(i * 255) for i in colorsys.hsv_to_rgb(h/360,s/100,v/100))
 			data[request.json['chan']] = "rgb" + str(rgb)
+			log = "Changing channel " + str(request.json['chan']) + " to hsv: " + str(h) + ", " + str(s) + ", " + str(v) +" using OSC Commands: " + "/cs/chan/select/" + str(request.json['chan']) + "      /cs/chan/at/" + str(v) + "      /cs/color/hs/" + str(h) + "/" + str(s)
+			d.append(log)
 		client.send_message("/cs/key/clearselection", " ")
-		with open('data.json', 'w') as outfile:
+		with open('json/data.json', 'w') as outfile:
 			json.dump(data, outfile)
+		with open('json/log.json', 'w') as logfile:
+			json.dump(list(d),logfile)
 		return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 	else:
 		return('Complete')
 if __name__== "__main__":
-	app.run(host='0.0.0.0', port=8006)
+	app.run(host='0.0.0.0', port=5000)
+
